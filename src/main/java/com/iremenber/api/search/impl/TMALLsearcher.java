04e5015da714a9949ff6389e7785a6239a4beecd
zhangming01 <zm474577048@qq.com>
2019-07-29 19:13:36 +0800

    增加天猫搜索

--- src/main/java/com/iremenber/api/search/impl/TMALLsearcher.java
+++ src/main/java/com/iremenber/api/search/impl/TMALLsearcher.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 import com.iremenber.api.search.Searcher;
 import com.iremenber.api.vo.Mall;
@@ -21,11 +23,24 @@ public class TMALLsearcher  implements Searcher{
 		try {
 			Document doc = Jsoup.connect("https://list.tmall.com/search_product.htm?q="+ keyWord)
 					.userAgent("Mozilla/5.0 (Windows NT 6.1; rv:6.0.2) Gecko/20100101 Firefox/6.0.2").get();
-			List<BigDecimal> prices = ((List<String>)doc.select("div.product p.productPrice em").eachAttr("title")).stream().map(p -> new BigDecimal(p)).collect(Collectors.toList());
-			List<String> names = doc.select("div.product p.productTitle a").eachText();
-			AtomicInteger i = new AtomicInteger(0);
-			return Mall.getTMall(names.stream().map((String name) ->{ 
-				Product p = new Product(name,prices.get(i.getAndIncrement()),null);
+//			List<BigDecimal> prices = ((List<String>)doc.select("div.product p.productPrice em").eachAttr("title")).stream().map(p -> new BigDecimal(p)).collect(Collectors.toList());
+//			productImg-wrap img  src
+//			List<String> names = doc.select("div.product p.productTitle a").eachText();
+//			AtomicInteger i = new AtomicInteger(0);
+//			return Mall.getTMall(names.stream().map((String name) ->{ 
+//				Product p = new Product(name,prices.get(i.getAndIncrement()),null);
+//				return p;
+//			}).collect(Collectors.toList()));
+			
+			
+			return Mall.getTMall(doc.select("div.product").stream().map( (Element e) -> {
+				Product p = new Product();
+				String price = e.select("p.productPrice em").attr("title");
+				if(StringUtils.isNotBlank(price)) {
+					p.setPrice(new BigDecimal(price.trim()));
+				}
+				p.setName(e.select("p.productTitle a").text());
+				p.setImg(e.select(".productImg-wrap img").attr("src"));
 				return p;
 			}).collect(Collectors.toList()));
 		} catch (IOException e) {
