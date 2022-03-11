e50fc542596dff50bcaa36114a2ea85dceb480b3
zhangming <zhangming@gg.315swy.cn>
2022-03-11 18:09:45 +0800

    sleuth

--- gateway/src/main/java/com/iremember/gateway/filters/ResponseFilter.java
+++ gateway/src/main/java/com/iremember/gateway/filters/ResponseFilter.java
@@ -2,10 +2,11 @@ package com.iremember.gateway.filters;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import brave.Tracer;
+
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
@@ -19,9 +20,9 @@ public class ResponseFilter {
 	public GlobalFilter postGlobalFilter() {
 		return (exchange, chain) -> {
 			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-				String traceId = tracer.currentSpan().context().traceIdString();
+				String traceId = tracer.currentSpan().context().traceId();
 				log.debug("Adding the correlation id to the outbound headers. {}", traceId);
-				exchange.getResponse().getHeaders().add("traceId", tracer.currentSpan().context().traceIdString());
+				exchange.getResponse().getHeaders().add("traceId", traceId);
 				log.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
 			}));
 		};
